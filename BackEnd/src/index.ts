5c57cb3cb04fc79bf8725a8495d04b6784c688f8
Eliedson <goncalveseliedson@gmail.com>
2025-03-27 07:31:29 -0300

    back end finished, frontend started

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -3,8 +3,8 @@ import { router } from "./routes/routes";
 import path from 'path';
 const config = JSON.parse(await Bun.file(path.join(__dirname, 'config', 'config.json')).text());
 
-const server = express()
+const Server = express()
     .use(express.json())
     .use('/api', router)
-    .listen(config.PORT, () => { console.log(`escutando na porta ${config.PORT}`) });
+    .listen(config.PORT, () => { console.log(`Listening on PORT: ${config.PORT}`) });
 
---
+++ BackEnd/src/utils/jsdom/indes.ts
@@ -0,0 +1,33 @@
+import type { AxiosResponse } from 'axios';
+import { JSDOM } from 'jsdom';
+
+
+export const scrapProducts = (response: AxiosResponse) => {
+    try {
+
+        if (!response.data) { throw new Error('No data found on response') }
+
+
+        const dom = new JSDOM(response.data);
+        const document = dom.window.document;
+        const products: any[] = [];
+
+        document.querySelectorAll(" .s-result-item ").forEach((item) => {
+            const title = item.querySelector("a h2 span")?.textContent || "not found";
+            const rating = item.querySelector(".a-icon-alt")?.textContent || "not found";
+            const reviews = item.querySelector(".a-size-small .s-underline-text")?.ariaLabel || "not found";
+            const imgSrc = item.querySelector("img")?.src || 'not found'
+
+
+            products.push({ title, rating, reviews, imgSrc });
+        });
+
+        return products
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            throw new Error(`Error: ${err.message}`)
+        } else {
+            throw new Error('Unknown Error')
+        }
+    }
+}
